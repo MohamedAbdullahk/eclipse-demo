03a281b3f273db917898c79d79f6edf1f7b640c8
abdullah <kmohamedabdullah@gmail.com>
2026-03-23 22:57:27 +0530

    First commit for demo purpose

---
+++ springboot-docker-kubernate/src/main/java/com/docker/k8s/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.docker.k8s.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class WelcomeController {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome docker kubernate with spring boot api version : 1.7";
+	}
+
+}
